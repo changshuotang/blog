17d61d63bee82d99f9ea8b0130e89435425da074
Eric Tang <cs.erictang@gmail.com>
2020-12-24 14:23:45 -0800

    Add basic tags page

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from "gatsby"
+const _ = require("lodash")
 
 const TagMetadata = {
     'Technical': {
@@ -11,17 +13,19 @@ const TagMetadata = {
 
 const Tag = ({ tagName }) => {
     return TagMetadata[tagName] && (
-        <small style={{
-            background: TagMetadata[tagName].color,
-            color: 'white',
-            paddingLeft: 4,
-            paddingRight: 4,
-            paddingTop: 2,
-            paddingBottom: 2,
-            borderRadius: 4,
-            marginLeft: 3,
-            marginRight: 3,
-        }}>{tagName}</small>
+        <Link style={{boxShadow: "none"}} to={`/tags/${_.kebabCase(tagName)}`}>
+            <small style={{
+                background: TagMetadata[tagName].color,
+                color: 'white',
+                paddingLeft: 4,
+                paddingRight: 4,
+                paddingTop: 2,
+                paddingBottom: 2,
+                borderRadius: 4,
+                marginLeft: 3,
+                marginRight: 3,
+            }}>{tagName}</small>
+        </Link>
     );
 };
 
